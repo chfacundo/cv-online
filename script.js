9c82c88be5f21df182441d7e922bcbc2bedfba6d
chfacundo <111554268+chfacundo@users.noreply.github.com>
2023-01-26 21:22:56 -0300

    Primera subida

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("photo1").onclick = function openPhoto() {
+    document.getElementById("clicked-image").style.visibility = "visible";
+}
+
+document.getElementById("close-button").onclick = function closePhoto() {
+    document.getElementById("clicked-image").style.visibility = "hidden";
+}
+
+
+
+
+
+
