b82be4a5ea549c888c202eada24b72f4044566e9
chfacundo <111554268+chfacundo@users.noreply.github.com>
2023-01-31 01:28:25 -0300

    v2

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
+// Abrir ventana de foto
 document.getElementById("photo1").onclick = function openPhoto() {
     document.getElementById("clicked-image").style.visibility = "visible";
 }
 
+// Cerrar ventana de foto
 document.getElementById("close-button").onclick = function closePhoto() {
     document.getElementById("clicked-image").style.visibility = "hidden";
 }
 
+//Enrollar/desenrollar información
+let rolledUp = false 
 
+document.getElementById("open-close-info").onclick = function rollUnroll() {
+    rolledUp = !rolledUp
+    console.log(rolledUp)
+    if (rolledUp) {
 
+        //ocultar información
+        document.getElementById("personal-info-body").removeAttribute("class")
+        document.getElementById("personal-info-body").className= "hidden"
+        document.getElementById("aside").style.backgroundColor = "var(--tertiary-color)"
 
+        //girar botón
+        document.getElementById("open-close-info").removeAttribute("class")
+        document.getElementById("open-close-info").className = "button-up"
+        document.getElementById("open-close-info").className = "open-close-info"
 
+    } else {
 
+        //mostrar información
+        document.getElementById("personal-info-body").removeAttribute("class")
+        document.getElementById("personal-info-body").className= "visible"
+        document.getElementById("personal-info-body").className= "personal-info-body"
+        document.getElementById("aside").removeAttribute("style")
+
+        //girar-botón 
+        document.getElementById("open-close-info").removeAttribute("class")
+        document.getElementById("open-close-info").className = "button-down"
+        document.getElementById("open-close-info").className = "open-close-info"
+    }
+}
